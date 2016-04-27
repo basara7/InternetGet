2ca97abd31e75529304516e3d484212e816b1c3f
basara7 <kirax10a6@126.com>
2016-04-27 22:22:05 +0800

    Initial Commit

---
+++ InternetGet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InternetGet
+//
+//  Created by Mao Mao on 16/4/27.
+//  Copyright © 2016年 Mao Mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InternetGet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InternetGet
+//
+//  Created by Mao Mao on 16/4/27.
+//  Copyright © 2016年 Mao Mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
